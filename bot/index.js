12df6dec3b517db13f85c11b231a56f71c159293
itskinshu <kinshuxrajput@gmail.com>
2024-11-05 13:23:13 +0530

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -5,101 +5,33 @@ const bot = new Telegraf(TOKEN);
 const express = require("express");
 const app = express()
 app.use(express.json())
-const web_link = "https://minati-app.netlify.app";
-const community_link = "https://t.me/firefarmer";
-const discussion_link = "https://t.me/firefarmer";
+const web_link = "https://minati.kissubots.in";
+const community_link = "https://t.me/Minativerseofficial";
+const discussion_link = "https://x.com/minatifi";
 bot.start((ctx) => {
     const startPayload = ctx.startPayload;
     const urlSent = `${web_link}?ref=${startPayload}`;
     const user = ctx.message.from;
     const userName = user.username ? `@${user.username}` : user.first_name;
-ctx.replyWithMarkdown(`*Hey, ${userName}🫰!*
-*Welcome to FireFarm* 🎉
+ctx.replyWithMarkdown(`*Hey, ${userName}🫰!*Welcome to Minativerse! 🌟 
 
-Tap the screen, collect coins 🌟, 
-grow up your passive income. 🚀
+Dive into the world of Minati, where your crypto journey expands beyond boundaries! 🌍 With our Minati debit card, metaverse platform, Minatibox wallet, and Tap&Earn Game, there’s always something exciting happening.
 
-🌟 Grab daily rewards 
-🎰 Spin the spinner machine
-🧟 Share with your buddies 
-🚀 Upgrade your rank 
+Join our community, complete quests, and earn fantastic rewards while exploring endless possibilities. 
 
+Got friends who love innovation? 👥 Invite them to explore Minati together! More friends = More adventures = Greater rewards! 🎉 
 
-`, {
+🚀 Powered by Minati `, {
     reply_markup: {
         inline_keyboard: [
-            [{ text: "✌️ Join our Community ✌️", url: community_link }],
-            [{ text: "🐥 How To Earn  🐥", callback_data: 'help' }],
+            [{ text: " Join our Community ", url: community_link }],
+            [{ text: " Our Twitter 💫", url: discussion_link }],
             [{ text: "🕹 Oepn App 🎮", web_app: { url: urlSent } }]
         ]
     }
 });
 });
 
-bot.action('help', (ctx) => {
-    const urlSent = `${web_link}?ref=${ctx.from.id}`;
-    ctx.replyWithMarkdown(`*What's the goal?*
-Earn Tokens,Upgrade Levels, 
-Refer To Friends and Do Tasks !
-AirDrop is coming soon...👀
-
-*💰Earn*
-🎰 Spin the slotmachine daily,
-Tap the screen to mine coins.
-
-*📈Profit per hour*
-Buy Cards And earn more profit,
-while you are not in the game. 
-
-*👥Friends*
-Invite friends to grow together! 
-get Bonuses and 10% of Earnings.
-
-*📋Tasks*
-Complete simple tasks every day 
-and receive rewards 🫰!
-`, {
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: "Join our Community", url: community_link }],
-                [{ text: "❄️ Join our Discussion ❄️", url: discussion_link }],
-                [{ text: "✨ Start now !", web_app: { url: urlSent } }]
-            ]
-        }
-    });
-});
-
-  
-bot.use((ctx, next) => {
-    const user = ctx.message.from;
-    const userName = user.username ? `@${user.username}` : user.first_name;
-ctx.replyWithMarkdown(`*Hey, ${userName}🫰!*
-*Welcome to FireFarm* 🎉
-
-Tap the screen, collect coins 🌟, 
-grow up your passive income. 🚀
-
-🌟 Grab daily rewards 
-🎰 Spin the spinner machine
-🧟 Share with your buddies 
-🚀 Upgrade your rank 
-
-
-`, {
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: " Join our Community ", url: community_link }],
-                [{ text: "🐥 How To Earn  🐥", callback_data: 'help' }],
-                [{ text: "🕹 Open App 🎮", web_app: { url: web_link } }]
-            ]
-        }
-    });
-    return next();
-});
-
-bot.on('text', (ctx) => {
-    // Handle text messages here
-});
 
 bot.launch().then(() => {
     console.log('Bot started successfully');
